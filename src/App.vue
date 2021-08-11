1b6339b3dabba03491e7d36241c1776a56ebf00d
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-08-12 03:50:56 +0900

    Calculator コンポーネントを切り出し

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  Hello, MyCalculator
+  <calculator />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Calculator from "./components/Calculator.vue";
 
 export default defineComponent({
   name: 'App',
   components: {
+    Calculator
   }
 })
 </script>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,13 @@
+<template>
+  This is Calculator Component
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
