8301de280aa255ae37d9a203c3c4cce82a37551b
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-08-12 05:02:39 +0900

    画面実装完了

---
+++ src/components/CalcButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="calcButton">
+    <div class="label">{{ label }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: "#503cd1",
+    },
+    hoveredColor: {
+      type: String,
+      default: "#3d2bb8",
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.calcButton {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  border-radius: 10px;
+  background-color: v-bind(color);
+  &:hover {
+    background-color: v-bind(hoveredColor);
+  }
+}
+</style>
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,14 +3,45 @@
     <div class="screen">
       <div class="answer">{{ number }}</div>
     </div>
-    <div class="controller">コントローラー</div>
+    <div class="buttons">
+      <CalcButton class="button" label="C" />
+      <CalcButton class="button" label="+=" />
+      <CalcButton class="button" label="%" />
+      <CalcButton class="button" label="/" />
+      <CalcButton class="button" label="7" />
+      <CalcButton class="button" label="8" />
+      <CalcButton class="button" label="9" />
+      <CalcButton class="button" label="X" />
+      <CalcButton class="button" label="4" />
+      <CalcButton class="button" label="5" />
+      <CalcButton class="button" label="6" />
+      <CalcButton class="button" label="-" />
+      <CalcButton class="button" label="1" />
+      <CalcButton class="button" label="2" />
+      <CalcButton class="button" label="3" />
+      <CalcButton class="button" label="+" />
+      <CalcButton class="button" label="0" />
+      <CalcButton class="button" label="." />
+      <CalcButton
+        class="button equal"
+        label="="
+        color="#f33d1d"
+        hoveredColor="#e4270f"
+      />
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from "vue";
+<script lang="ts">
+import { defineComponent } from "vue";
+import CalcButton from "./CalcButton.vue";
 
-const number = ref(0);
+export default defineComponent({
+  components: { CalcButton },
+  data: () => ({
+    number: 0,
+  }),
+});
 </script>
 
 <style lang="scss" scoped>
@@ -37,9 +68,20 @@ const number = ref(0);
     }
   }
 
-  .controller {
+  .buttons {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: repeat(5, 1fr);
+    grid-gap: 10px;
     width: 100%;
     height: calc(100% - 110px);
+    .button {
+      color: #fff;
+      font-size: 24px;
+      &.equal {
+        grid-column: 3/5;
+      }
+    }
   }
 }
 </style>
